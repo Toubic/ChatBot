f221bbadd3dceb8a82c9de82c2e7b9bda0168126
Toubic <toubicgmail.com>
2021-03-01 16:19:36 +0100

    edit gitignore

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 var Bot = require("./Bot.js");
 
 Bot.Connect(); //Connect and connect message
 
 Bot.ChatCommands(); // !Commands
 
+=======
+var Bot = require("./Bot.js");
+
+Bot.Connect(); //Connect and connect message
+
+Bot.ChatCommands(); // !Commands
+
+>>>>>>> 1b61a2d (latest changes)
 Bot.ChatCommandsInterval(); // Interval commands
