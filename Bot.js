66bb2fe96aec56fecb4552d7c546a25147e5365e
Toubic <toubicgmail.com>
2021-03-01 16:28:33 +0100

    latest changes

--- Bot.js
+++ Bot.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 require('dotenv').config()
-var SongRequests = require("./SongRequests.js");
+let SongRequests = require("./SongRequests.js");
 const tmi = require("tmi.js");
 const Scry = require("scryfall-sdk");
 
@@ -33,7 +32,7 @@ module.exports.Connect = function(){
 };
 
 function getCommand(sMessage){
-    var sCommand;
+    let sCommand;
 
     sCommand = sMessage.trim();
 
@@ -45,7 +44,7 @@ function getCommand(sMessage){
 };
 
 module.exports.ChatCommands = function(){
-    var sCommand;
+    let sCommand;
 
     client.on("chat", (channel,user,message,self) => {
         sCommand = getCommand(message);
@@ -106,112 +105,3 @@ module.exports.ChatCommandsInterval = function(){
 };
 
 // user name client.action("Twibbe",user["display-name"]);
-=======
-require('dotenv').config()
-var SongRequests = require("./SongRequests.js");
-const tmi = require("tmi.js");
-const Scry = require("scryfall-sdk");
-
-const options = {
-    options: {
-        debug: true,
-
-    },
-    connection: {
-        cluster: "aws",
-        reconnect: true,
-    },
-    identity: {
-        username: "TwibbeBot",
-        password: process.env.OAUTH_TOKEN,
-    },
-    channels: ["Twibbe"],
-};
- 
-const client = new tmi.client(options);1
-
-module.exports.Connect = function(){
-
-    client.connect(); // Connect
-
-    // Conect message
-    client.on("connected",(address,port) => {
-        client.action(options.channels[0], "Hello chat! imGlitch")
-    });
-};
-
-function getCommand(sMessage){
-    var sCommand;
-
-    sCommand = sMessage.trim();
-
-    if(sCommand.indexOf(" ") > 0){
-        sCommand = sCommand.slice(0,sCommand.indexOf(" "));
-    }
-
-    return sCommand;
-};
-
-module.exports.ChatCommands = function(){
-    var sCommand;
-
-    client.on("chat", (channel,user,message,self) => {
-        sCommand = getCommand(message);
-        
-        switch(sCommand){
-            case "!hype":
-                client.action(options.channels[0],"TwitchLit TwitchLit TwitchLit TwitchLit TwitchLit TwitchLit");
-                break;
-            case "!rules":
-                client.action(options.channels[0],"- Be friendly and use common sense.");
-                client.action(options.channels[0],"- No politics/religion/NSFW.");
-                break;
-            case "!hey":
-                client.action(options.channels[0]," Hey " + message.trim().slice(5) + " HeyGuys");
-                break;
-            case "!so":
-                client.action(options.channels[0],"Check out this awesome streamer ->  www.twitch.tv/" + message.trim().slice(4));
-                break;
-            case "!github":
-                client.action(options.channels[0],"Check out my GitHub page -> github.com/Toubic");
-                break;
-            case "!discord":
-                    // client.action(options.channels[0],"Check out the Discord -> https://discord.gg/SjHtM4sW"); find permananent adress
-                    break;
-            case "!gg":
-                client.action(options.channels[0],"GG!  HSWP SeemsGood");
-                break;
-            case "!rcg":
-                Scry.Cards.random().then(result => client.action(options.channels[0],result.name + " (" + result.set + ")")); 
-                break;
-            case "!rcqg":
-                Scry.Cards.random().then(result => client.action(options.channels[0],result.flavor_text)); 
-                break;
-            case "!card":
-                message = message.slice(5).trim();
-                Scry.Cards.byName(message).then((result) => { 
-                    client.action(options.channels[0],result.name + " (" + result.set + ") " + result.oracle_text + " " + result.flavor_text); 
-                });
-                break;
-            case "!song": // TODO
-                SongRequests.addSong();
-                break;
-            default:
-                //client.action("Twibbe",user["display-name"] + " that command does not exist.");
-        }
-    });
-};
-
-module.exports.ChatCommandsInterval = function(){
-    /*
-    setInterval(function(){ 
-        client.action(options.channels[0],"/clear");
-    }, 900000);
-    */
-    setInterval(function(){ 
-        client.action(options.channels[0],"Please leave a follow if you like the stream.");
-    }, 3600000);        
-};
-
-// user name client.action("Twibbe",user["display-name"]);
->>>>>>> 1b61a2d (latest changes)
--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 var Bot = require("./Bot.js");
 
 Bot.Connect(); //Connect and connect message
 
 Bot.ChatCommands(); // !Commands
 
-=======
-var Bot = require("./Bot.js");
-
-Bot.Connect(); //Connect and connect message
-
-Bot.ChatCommands(); // !Commands
-
->>>>>>> 1b61a2d (latest changes)
 Bot.ChatCommandsInterval(); // Interval commands
